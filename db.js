527e1e270bf77a13b6ac7ba1fff9770b399f0d40
Freebz1122 <dfreeborough@live.co.uk>
2025-06-23 15:49:39 +0100

    Fix Dexie import in db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-import { Dexie } from 'https://unpkg.com/dexie@3.2.2/dist/dexie.js';
+import { Dexie } from 'https://unpkg.com/dexie@3.2.2/dist/dexie.min.js';
 
 const db = new Dexie('BunkerSyncDB');
 db.version(1).stores({
-  tasks: '++id,courseId,bunkerId,description,type,status,timestamp',
-  bunkers: '++id,courseId,holeNumber,lat,lng',
-  courses: 'id,name,maps'
+  courses: 'id, name, maps',
+  bunkers: 'id, courseId, holeNumber, lat, lng',
+  tasks: 'id, courseId, bunkerId, description, type, status, timestamp'
 });
 
 export { db };
